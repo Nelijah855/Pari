23ba53fa2d18993e97c145b8f37dc20988c69384
Nelijah855 <nelijah855@gmail.com>
2024-06-12 12:53:26 -0700

    Add files via upload

---
+++ inner.py
@@ -0,0 +1,149 @@
+
+
+
+import kivy 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+class Inner(FloatLayout):
+    
+    def a(self):
+        code=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.2})
+        
+        l1=Label(text='usercode ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.18})
+        #variable to hold the ammount of money to be sold
+        a_m=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.3})
+        l2=Label(text='name ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.28})
+        #variable to hold the currency
+        ugx=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.4})
+        l3=Label(text='trust ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.38})
+        #variable to hold the sellers name 
+        user_id=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.5})
+        l4=Label(text='number',size_hint=(.1,.1),pos_hint={'x':.8,'y':.48})
+
+        but=Button(text='send',size_hint=(.1,.05),pos_hint={'x':.4,'y':.08})
+
+        
+        self.add_widget(code)
+        self.add_widget(l1)
+        self.add_widget(l2)
+        self.add_widget(l3)
+        self.add_widget(l4)
+        self.add_widget(a_m)
+        self.add_widget(ugx)
+        self.add_widget(user_id)
+        self.add_widget(but)
+
+
+
+    def b(self):
+        code=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.2})
+        
+        l1=Label(text='usercode ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.18})
+        #variable to hold the ammount of money to be sold
+        a_m=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.3})
+        l2=Label(text='name ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.28})
+        #variable to hold the currency
+        ugx=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.4})
+        l3=Label(text='trust ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.38})
+        #variable to hold the sellers name 
+        user_id=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.5})
+        l4=Label(text='number',size_hint=(.1,.1),pos_hint={'x':.8,'y':.48})
+
+        but=Button(text='send',size_hint=(.1,.05),pos_hint={'x':.4,'y':.08})
+
+        
+        self.add_widget(code)
+        self.add_widget(l1)
+        self.add_widget(l2)
+        self.add_widget(l3)
+        self.add_widget(l4)
+        self.add_widget(a_m)
+        self.add_widget(ugx)
+        self.add_widget(user_id)
+        self.add_widget(but)
+        
+
+
+    def c(self):
+        code=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.2})
+        
+        l1=Label(text='usercode ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.18})
+        #variable to hold the ammount of money to be sold
+        a_m=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.3})
+        l2=Label(text='name ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.28})
+        #variable to hold the currency
+        ugx=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.4})
+        l3=Label(text='trust ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.38})
+        #variable to hold the sellers name 
+        user_id=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.5})
+        l4=Label(text='number',size_hint=(.1,.1),pos_hint={'x':.8,'y':.48})
+
+        but=Button(text='send',size_hint=(.1,.05),pos_hint={'x':.4,'y':.08})
+
+        
+        self.add_widget(code)
+        self.add_widget(l1)
+        self.add_widget(l2)
+        self.add_widget(l3)
+        self.add_widget(l4)
+        self.add_widget(a_m)
+        self.add_widget(ugx)
+        self.add_widget(user_id)
+        self.add_widget(but)
+
+
+    def d(self):
+        code=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.2})
+        
+        l1=Label(text='usercode ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.18})
+        #variable to hold the ammount of money to be sold
+        a_m=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.3})
+        l2=Label(text='name ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.28})
+        #variable to hold the currency
+        ugx=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.4})
+        l3=Label(text='trust ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.38})
+        #variable to hold the sellers name 
+        user_id=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.5})
+        l4=Label(text='number',size_hint=(.1,.1),pos_hint={'x':.8,'y':.48})
+
+        but=Button(text='send',size_hint=(.1,.05),pos_hint={'x':.4,'y':.08})
+
+        
+        self.add_widget(code)
+        self.add_widget(l1)
+        self.add_widget(l2)
+        self.add_widget(l3)
+        self.add_widget(l4)
+        self.add_widget(a_m)
+        self.add_widget(ugx)
+        self.add_widget(user_id)
+        self.add_widget(but)
+
+    def e(self):
+        code=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.2})
+        
+        l1=Label(text='usercode ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.18})
+        #variable to hold the ammount of money to be sold
+        a_m=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.3})
+        l2=Label(text='name ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.28})
+        #variable to hold the currency
+        ugx=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.4})
+        l3=Label(text='trust ',size_hint=(.1,.1),pos_hint={'x':.8,'y':.38})
+        #variable to hold the sellers name 
+        user_id=TextInput(size_hint=(.3,.05),pos_hint={'x':.4,'y':.5})
+        l4=Label(text='number',size_hint=(.1,.1),pos_hint={'x':.8,'y':.48})
+
+        but=Button(text='send',size_hint=(.1,.05),pos_hint={'x':.4,'y':.08})
+
+        
+        self.add_widget(code)
+        self.add_widget(l1)
+        self.add_widget(l2)
+        self.add_widget(l3)
+        self.add_widget(l4)
+        self.add_widget(a_m)
+        self.add_widget(ugx)
+        self.add_widget(user_id)
+        self.add_widget(but)
